b05d4f75312956c9c57c79119934da27c8d4b5d9
Maxim_Forys <42719558+Maxim-Forys@users.noreply.github.com>
2020-11-21 20:23:17 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 const menuToggle = document.querySelector('#menu-togle');
 const mobileNavContainer = document.querySelector('#mobile-container');
 const body = document.querySelector('body');
+const nav = document.querySelector('.nav-menu');
+
 
 menuToggle.onclick = function(){
     menuToggle.classList.toggle('menu-icon-active');
     mobileNavContainer.classList.toggle('mobile-container-active');
     body.classList.toggle('lock');
+    nav.classList.toggle('nav-menu-active');
+}
+
+nav.onclick = function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-container-active');
+    body.classList.toggle('lock');
+    nav.classList.toggle('nav-menu-active');
+}
+
+mobileNavContainer.onclick = function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-container-active');
+    body.classList.toggle('lock');
+    nav.classList.toggle('nav-menu-active');
 }
